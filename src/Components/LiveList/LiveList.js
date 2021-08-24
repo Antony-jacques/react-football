638e96e3e8240f94052d7271d485ce1aeb586ba0
Antony <antony.jacques@laposte.net>
2021-08-24 07:55:51 +0200

    Tchat init

---
+++ src/Components/LiveList/LiveList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LiveList() {
+    return (
+        <div>
+            LiveList
+        </div>
+    )
+}
--- src/Pages/PrivateLivePage/PrivateLivePage.js
+++ src/Pages/PrivateLivePage/PrivateLivePage.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import PrivateNavbar from '../../Components/PrivateNavbar/PrivateNavbar'
-
+import LiveList from '../../Components/LiveList/LiveList'
+import Tchat from '../../Components/Tchat/Tchat'
 
 export default function PrivateLivePage() {
     return (
         <div>
             <PrivateNavbar/>
             PrivateLivePage
+            <LiveList/>
+            <Tchat/>
         </div>
     )
 }
