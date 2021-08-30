c6c23cecbe0bf48a232c88813daddd7696698dc7
Antony <antony.jacques@laposte.net>
2021-08-30 16:29:48 +0200

    showing messages

--- src/Components/Tchat/Tchat.js
+++ src/Components/Tchat/Tchat.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import firebase from '../../firebase'
+import {db} from '../../firebase'
 
 export default function Tchat() {
-    return (
-        <div>
-            Tchat
-        </div>
-    )
+  const [author, setAuthor] = useState("");
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+      //acceder à la table messages
+    db.collection('messages').orderBy('createdAt').limit(25).onSnapshot((snapshot)=>{
+        setMessages(snapshot.docs.map(doc=>doc.data()))
+    })
+  },[])
+
+//   const createMessage = (e) => {
+//       e.preventDefault();
+//     // on renseigne la DB qu'on utilise dans ref()
+//     const messagesDB = firebase.database().ref("messagesDB");
+//     const message = {
+//       author,
+//       text,
+//     };
+
+//     messagesDB.push(message);
+
+//     setAuthor("");
+//     setText("");
+//   };
+
+console.log(messages)
+  return (
+    <div>
+        <h4>Chat en direct</h4>
+        {messages.map(({id, text})=>(
+            <p key={id}>
+                {text}
+            </p>
+        ))}
+        {text}
+      {/* <form onSubmit={createMessage}>
+        <textarea placeholder="message" type="text" value={text} onChange ={(e)=>{setText(e.target.value)}}/>
+        <button type="subit">Envoyer</button>
+      </form> */}
+    </div>
+  );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,18 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore';
+// import 'firebase/firestore'
 
 const app = firebase.initializeApp({
     apiKey: "AIzaSyB-QytXbDHgrC5LMvOLpBJ7rHCT-77F9K4",
     authDomain: "react-football-41714.firebaseapp.com",
-    databaseURL: "https://react-football-41714-default-rtdb.europe-west1.firebasedatabase.app",
+    // databaseURL: "https://react-football-41714-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "react-football-41714",
     storageBucket: "react-football-41714.appspot.com",
     messagingSenderId: "172086194853",
     appId: "1:172086194853:web:66b06b4de820ffdfe8934c"
 })
-
+const db = app.firestore()
 export const auth = app.auth()
+export {db}
 export default app
