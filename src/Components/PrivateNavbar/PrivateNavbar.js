9e148fbde78ecc5c05e2babaee4cd82cbd3a4fa2
Antony <antony.jacques@laposte.net>
2021-09-17 12:38:34 +0200

    PrivateNavbar style

--- src/Components/PrivateNavbar/PrivateNavbar.js
+++ src/Components/PrivateNavbar/PrivateNavbar.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useContext} from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 
 import { Link } from "react-router-dom";
 
+import {AuthContext} from '../../Context/AuthContext'
+import './PrivateNavbar.css'
+
 export default function PrivateNavbar() {
+
+  const {logout} = useContext(AuthContext)
+  const navItems = [
+    {
+      url: "/accueil",
+      text: "Résumés vidéo",
+    },
+    {
+      url: "/live",
+      text: "Live",
+    },
+    {
+      url: "/classements",
+      text: "Classements",
+    },
+    {
+      url: "/profile",
+      text: "Mon profil",
+    },
+  ];
   return (
     <Navbar bg="light" expand="lg" className="navbar">
       <Container>
@@ -13,18 +36,20 @@ export default function PrivateNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link style={{ margin: "0 3rem" }} to="/accueil">
-              Highlights
-            </Link>
-            <Link style={{ margin: "0 3rem" }} to="/live">
-              Live
-            </Link>
-            <Link style={{ margin: "0 3rem" }} to="/profile">
-            Profil
-            </Link>
-            <Link style={{ margin: "0 3rem" }} to="/classements">
-            Classements
-            </Link>
+
+
+            {navItems.map((item, index) => (
+              
+              <Link style={{ color:'rgba(0,0,0,.9);' ,   padding:'0rem 2rem' ,   textDecoration: 'none' }} to={item.url}>
+               <Nav.Link >
+                {item.text}
+                 </Nav.Link>
+              </Link>
+            ))}
+            <Nav.Link >
+            <Link onClick={logout} style={{ margin: "0 3rem",textDecoration: 'none' }}>Se déconnecter</Link>
+
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
